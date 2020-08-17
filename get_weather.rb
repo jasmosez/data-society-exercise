75416563f78c98696cb811daf26fd1e246fa2480
jasmosez <jas.schaffer@gmail.com>
2020-08-17 11:43:44 -0400

    first commit

---
+++ get_weather.rb
@@ -0,0 +1,5 @@
+# Uses this free weather service to get weather data about each location:
+# https://www.weather.gov/documentation/services-web-api
+# Extract the "temperature" value for "Wednesday Night" for each location
+# Writes out the results to a local text file, in this format:
+# 72, 68, 70
